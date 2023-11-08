f5e32d7bba99b5259743f04a26f901c2ee06ad62
debuggersolutions <debuggersolutions@gmail.com>
2023-11-08 16:24:44 +0530

    first

---
+++ admin/layout/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link rel="stylesheet" href="http://localhost/major-project/admin/assets/css/style.css">
+</head>
+
+<body>
+      <div class="body">
+            <aside class="sidebar">
+                  <div class="logo">
+                        <h1>LOGO</h1>
+                  </div>
+                  <ul>
+                        <li><a href="http://localhost/major-project/admin/index.php">Dashboard</a></li>
+                        <li> <a href="http://localhost/major-project/admin/products/category.php">Category</a></li>
+                        <li> <a href="http://localhost/major-project/admin/products/product.php">Product</a></li>
+                  </ul>
+            </aside>
+
+
+            <div class="content-area">
+                  <header>
+                        <ul>
+                              <li><a href="">Home</a></li>
+                              <li><a href="">Logout</a></li>
+                        </ul>
+                  </header>
+                  <div class="content">
---
+++ admin/products/category.php
@@ -0,0 +1,9 @@
+<?php
+include('../layout/header.php')
+?>
+
+<h1>category</h1>
+
+<?php
+include('../layout/footer.php')
+?>
